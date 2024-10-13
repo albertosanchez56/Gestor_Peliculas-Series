a220536ab53ce87c70695c59cd54368646fb8b4f
Alberto Sanchez <albertosanchez15111rgmail.com>
2024-10-13 22:42:01 +0200

    gateway-service

--- eureka-service3/src/main/java/com/eureka/service3/EurekaService3Application.java
+++ eureka-service3/src/main/java/com/eureka/service3/EurekaService3Application.java
@@ -2,8 +2,10 @@ package com.eureka.service3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaService3Application {
 
 	public static void main(String[] args) {
