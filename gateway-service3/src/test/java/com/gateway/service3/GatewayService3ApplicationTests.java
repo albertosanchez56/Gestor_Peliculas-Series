efb5e1ecad8d2eeeb6cd9dbc91f06db82e2ad467
Alberto Sanchez <albertosanchez15111rgmail.com>
2024-10-13 22:31:39 +0200

    gateway-service

---
+++ gateway-service3/src/test/java/com/gateway/service3/GatewayService3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.service3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayService3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
