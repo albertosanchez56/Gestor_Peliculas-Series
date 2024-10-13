13b2294b30fca3399b80bf7e5dca602ec82af8a1
Alberto Sanchez <albertosanchez15111@gmail.com>
2024-10-13 22:31:39 +0200

    gateway-service

---
+++ eureka-service3/src/main/java/com/eureka/service3/EurekaService3Application.java
@@ -0,0 +1,13 @@
+package com.eureka.service3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaService3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaService3Application.class, args);
+	}
+
+}
