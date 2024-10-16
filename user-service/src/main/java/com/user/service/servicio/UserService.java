8bdc1fdbacff1f5492c7c8f82d61ba3aeb5e2c60
Alberto Sanchez <albertosanchez15111@gmail.com>
2024-10-16 23:29:33 +0200

    get y post de usuario

--- user-service/src/main/java/com/user/service/servicio/UserService.java
+++ user-service/src/main/java/com/user/service/servicio/UserService.java
@@ -1,8 +1,33 @@
 package com.user.service.servicio;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.user.service.Entidades.User;
+import com.user.service.repositorio.UserRepository;
 
 @Service
 public class UserService {
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> getAll(){
+		return userRepository.findAll();
+	}
+	
+	public User obtenerUsurario(int id) {
+		return userRepository.findById(id).orElse(null);
+	}
+	
+	public User save(User usuario) {
+		User nuevoUsuario = userRepository.save(usuario);
+		return nuevoUsuario;
+	}
 }
