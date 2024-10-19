c9711a4f547ddcdf5069f15ad78be8bbc857e49a
Alberto Sanchez <albertosanchez15111rgmail.com>
2024-10-19 17:39:36 +0200

    index

---
+++ gateway-service3/src/main/java/com/gateway/service3/controller/GatewayController.java
@@ -0,0 +1,18 @@
+package com.gateway.service3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GatewayController {
+
+	 @GetMapping("/Home")
+	    public String showIndex(Model model) {
+	        // Agregar cualquier dato que necesites pasar a la vista
+	        model.addAttribute("title", "Bienvenido a mi portfolio");
+	        
+	        // Devolver el nombre de la vista Thymeleaf (index.html)
+	        return "index";
+	    }
+}
