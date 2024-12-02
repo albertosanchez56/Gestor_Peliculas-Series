37b1946becd8067c23ab342056dbf250a81fe6f7
Alberto Sanchez <albertosanchez15111rgmail.com>
2024-12-02 21:31:51 +0100

    solucion de redireccion de metodo post

--- gateway-service3/src/main/java/com/gateway/service3/controller/DirectorGatewayController.java
+++ gateway-service3/src/main/java/com/gateway/service3/controller/DirectorGatewayController.java
@@ -1,5 +1,7 @@
 package com.gateway.service3.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
 
 import com.gateway.service3.client.DirectorClient;
 import com.gateway.service3.modelos.Director;
@@ -27,7 +32,7 @@ public class DirectorGatewayController {
 
     private final String peliculasServiceUrl = "http://localhost:9090/peliculas/directores";
 
-    @PostMapping("/guardar-director")
+    /*@PostMapping("/guardar-director")
     public Mono<String> guardarDirector(@ModelAttribute Director director) {
         return webClientBuilder.build()
             .post()
@@ -35,6 +40,25 @@ public class DirectorGatewayController {
             .bodyValue(director)
             .retrieve()
             .bodyToMono(String.class)
-            .map(response -> "redirect:/guardardirector");  // Redirigir de manera reactiva
+            .map(response -> "redirect:/peliculasinfo");  // Redirigir de manera reactiva
+    }*/
+    
+    public DirectorGatewayController(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    @PostMapping("/guardar-director")
+    public Mono<Void> guardarDirector(@ModelAttribute Director director, ServerWebExchange exchange) {
+        return webClientBuilder.build()
+            .post()
+            .uri(peliculasServiceUrl)
+            .bodyValue(director)
+            .retrieve()
+            .bodyToMono(String.class)
+            .then(Mono.defer(() -> {
+                exchange.getResponse().setStatusCode(HttpStatus.FOUND);
+                exchange.getResponse().getHeaders().setLocation(URI.create("/AgregarDirectores"));
+                return exchange.getResponse().setComplete();
+            }));
     }
 }
