0b90439107a3d02c5eb4c46fac52fece2ee646f1
Alberto Sanchez <albertosanchez15111@gmail.com>
2024-12-03 19:59:32 +0100

    intento de uso de feign fallido

--- gateway-service3/src/main/java/com/gateway/service3/GatewayService3Application.java
+++ gateway-service3/src/main/java/com/gateway/service3/GatewayService3Application.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@EnableFeignClients
 @ComponentScan(basePackages = {"com.gateway.service3", "com.gateway.service3.configuracion"})
 public class GatewayService3Application {
 
