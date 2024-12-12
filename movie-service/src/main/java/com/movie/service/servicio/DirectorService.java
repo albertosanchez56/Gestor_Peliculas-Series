2599415e9850eb29a13e63acd661a53cee5bfee1
Alberto Sanchez <albertosanchez15111rgmail.com>
2024-12-12 19:21:42 +0100

    agregadas funcionalidades de borrar y actualizar director

--- movie-service/src/main/java/com/movie/service/servicio/DirectorService.java
+++ movie-service/src/main/java/com/movie/service/servicio/DirectorService.java
@@ -1,6 +1,7 @@
 package com.movie.service.servicio;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,12 +24,20 @@ public class DirectorService {
 		return directorRepository.findAll();
 	}
 	
-	public Director obtenerDirector(int id) {
+	/*public Director obtenerDirector(int id) {
 		return directorRepository.findById(id).orElse(null);
+	}*/
+	
+	public Optional<Director> obtenerDirector(int id) {
+	    return directorRepository.findById(id);
 	}
 	
 	public Director save(Director director){
 		Director nuevoDirector = directorRepository.save(director);
 		return nuevoDirector;
 	}
+	
+	public void borrarDirector(Director director) {
+        directorRepository.delete(director);
+    }
 }
